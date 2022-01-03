43877d6340f728c075c7c8187d19268ed5e87396
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-03 21:21:33 +0545

    uo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home'
+import './App.css';
+import { Route } from 'react-router-dom';
+
+const App = ()=> {
+  return (
+    <>
+    <Home />
+    </>
+  );
+};
+
+export default App;
