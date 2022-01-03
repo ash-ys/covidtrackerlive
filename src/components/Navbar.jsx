de5045570787a4855f5877b18a983a2242db6c4c
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-03 16:46:14 +0545

    start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to ="/" className= "navbar-logo"></Link>
+                </div> 
+            </nav>
+            </>
+        </div>
+    )
+}
+
+export default Navbar
