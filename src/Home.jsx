8521eb39040307ac530afd69bba1dcdcbe333b0b
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-06 14:47:33 +0545

    card

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 
 function App() {
   return (
     <div>
-    Hi
+    <CardGroup>
+  <Card bg = "primary" text= "white" className='text-center' border="success" style={{ margin: '10px' }}>
+    
+    <Card.Body>
+      <Card.Title>Cases</Card.Title>
+      <Card.Text>
+        109
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small >Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card bg = "danger" text= "white"  className='text-center' border="primary" style={{ margin: '10px' }}>
+  
+    <Card.Body>
+      <Card.Title>Deaths</Card.Title>
+      <Card.Text>
+        0
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small >Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card bg = "success" text= "white"  className='text-center' border="danger" style={{ margin: '10px' }}>
+
+    <Card.Body>
+      <Card.Title>Recover</Card.Title>
+      <Card.Text>
+        99
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small>Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
     </div>
   );
 };
