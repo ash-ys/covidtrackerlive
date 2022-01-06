35efe18d68ddd1e059498a4063dbcff88eb2d0a4
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-06 16:49:48 +0545

    covid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, CardGroup, CardColumns } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
@@ -7,16 +7,19 @@ import axios from 'axios';
 function App() {
   const [latest, setLatest] = useState([]);
   const [results, setResults] = useState([])
+  const [nepal, setNepal] = useState([])
   useEffect(()=> {
     axios
     .all([
       axios.get("https://disease.sh/v3/covid-19/all"),
-      axios.get("https://disease.sh/v3/covid-19/countries")
+      axios.get("https://disease.sh/v3/covid-19/countries"),
+      axios.get("https://disease.sh/v3/covid-19/countries/nepal")
     ])
     
     .then (responseArr =>{
       setLatest(responseArr[0].data);
       setResults(responseArr[1].data);
+      setNepal(responseArr[2].data);
     })
     .catch(err => {
       console.log(err);
@@ -35,7 +38,8 @@ function App() {
     border="success"
      style={{ margin: '10px' }}
      >
-    <Card.Img variants = "top" src= {data.countryInfo.flag} />
+    <Card.Img variants = "top" src= {data.countryInfo.flag} style={{ maxWidth: '100%',
+    height: 'auto'}} />
     <Card.Body>
       <Card.Title>{data.country}</Card.Title>
       <Card.Text>
@@ -62,7 +66,7 @@ function App() {
 
   return (
     <div>
-    <CardGroup>
+    <CardGroup style={{backgroundColor:'#D9D7F1', display:'grid', gridTemplateColumns:'auto auto auto',}}>
   <Card bg = "primary" text= "white" className='text-center' border="success" style={{ margin: '10px' }}>
     
     <Card.Body>
@@ -99,8 +103,40 @@ function App() {
       <small>Last updated {lastUpdated}</small>
     </Card.Footer>
   </Card>
+  <Card 
+  
+  bg = "light"
+   text= "dark" 
+   className='text-center'
+    border="success"
+     style={{ margin: '10px' }}
+     >
+    <Card.Img variants = "top" src= "https://disease.sh/assets/img/flags/np.png" style={{ maxWidth: '100%',
+    height: 'auto'}} />
+    <Card.Body>
+      <Card.Title>{nepal.country}</Card.Title>
+      <Card.Text>
+        Cases {nepal.cases}
+      </Card.Text>
+      <Card.Text>
+        Deaths {nepal.deaths}
+      </Card.Text>
+      <Card.Text>
+        Recovered {nepal.recovered}
+      </Card.Text><Card.Text>
+        Today's cases {nepal.todaycases}
+      </Card.Text><Card.Text>
+        Today's deaths {nepal.todaydeaths}
+      </Card.Text><Card.Text>
+        Active {nepal.active}
+      </Card.Text><Card.Text>
+      Critical {nepal.critical}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  {countries}
 </CardGroup>
-{countries}
+
     </div>
   );
 };
